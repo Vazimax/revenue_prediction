f70207e089e8d0d2b3a48fd2a11e33e015143ed7
aboubakr el habti <aboubakr.elhabti@gmail.com>
2023-02-16 02:27:09 +0800

    linked the ml model to flask

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import pickle
-
 from flask import Flask, render_template, request
 
 app = Flask(__name__) 
+model = pickle.load(open('model.pkl','rb'))
+@app.route('/')
+def index():
+
+    return render_template("home.html")
+@app.route('/predict',methods=['GET','POST'])
+def predict():
+    new_var = float(request.form.get('temperature'))
+    prediction = model.predict([[new_var]])
+    output = round(prediction[0],2)
+    return render_template("home.html",text=f"The predicted revenue is {output}")
+
+if __name__ == '__main__':
+    app.run(debug=True)
